d478652f54699e009c39eeca311938d42d53cc96
craigdallimore <decoy9697@gmail.com>
2022-04-04 09:43:34 +1200

    Fix sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,19 @@ import typescript from "@rollup/plugin-typescript";
 import serve from "rollup-plugin-serve";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import sourcemaps from "rollup-plugin-sourcemaps";
 import livereload from "rollup-plugin-livereload";
 
 export default {
   input: "src/main.ts",
   output: {
+    sourcemap: true,
     file: "static/bundle.js",
   },
   plugins: [
-    typescript(),
+    typescript({ sourceMap: true, inlineSources: true }),
     serve("static"),
     commonjs(),
     resolve(),
-    sourcemaps(),
     livereload(),
   ],
 };
