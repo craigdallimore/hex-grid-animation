cdded424754e3129fcb704c46193ff73ca3c96bc
craigdallimore <decoy9697@gmail.com>
2022-04-04 11:36:34 +1200

    Begin workshopping presentation

--- src/drawScene.ts
+++ src/drawScene.ts
@@ -4,15 +4,7 @@ import type { Grid, Point, State, Path } from "./state";
 function drawPoint(ctx: CanvasRenderingContext2D, point: Point) {
   ctx.save();
   ctx.fillStyle = "hotpink";
-  ctx.fillRect(point.x, point.y, 3, 3);
-
-  ctx.fillStyle = "#333333";
-  ctx.font = "6pt sans-serif";
-  ctx.fillText(`C${point.col}:R${point.row}`, point.x + 10, point.y + 10);
-  ctx.fillText(`C${point.col}:R${point.row}`, point.x - 30, point.y + 10);
-  ctx.fillText(`C${point.col}:R${point.row}`, point.x + 10, point.y - 3);
-  ctx.fillText(`C${point.col}:R${point.row}`, point.x - 30, point.y - 3);
-
+  ctx.fillRect(point.x, point.y, 1, 1);
   ctx.restore();
 }
 
@@ -22,12 +14,19 @@ function drawGrid(ctx: CanvasRenderingContext2D, grid: Grid) {
   });
 }
 
-function drawTraveller(ctx: CanvasRenderingContext2D, state: State) {
-  const distance = state.travel * state.path.length;
-  const [x, y] = getPointOnPath(state.path, distance);
+function drawTraveller(
+  ctx: CanvasRenderingContext2D,
+  state: State,
+  offset: number,
+  colour: string
+) {
+  const d1 =
+    state.travel > offset ? (state.travel - offset) * state.path.length : 0;
+
+  const [x, y] = getPointOnPath(state.path, d1);
   ctx.save();
-  ctx.fillStyle = "rebeccapurple";
-  ctx.fillRect(x - 6, y - 6, 12, 12);
+  ctx.fillStyle = colour;
+  ctx.fillRect(x - 0.5, y - 0.5, 1, 1);
   ctx.restore();
 }
 
@@ -49,9 +48,11 @@ export default function drawScene(
   ctx: CanvasRenderingContext2D,
   state: State
 ): void {
-  ctx.clearRect(0, 0, state.ui.width, state.ui.height);
-
-  drawGrid(ctx, state.grid);
-  drawPath(ctx, state.path);
-  drawTraveller(ctx, state);
+  //ctx.clearRect(0, 0, state.ui.width, state.ui.height);
+  //drawGrid(ctx, state.grid);
+  //drawPath(ctx, state.path);
+  drawTraveller(ctx, state, 0, "red");
+  drawTraveller(ctx, state, 1, "orange");
+  drawTraveller(ctx, state, 2, "yellow");
+  drawTraveller(ctx, state, 7, "black");
 }
